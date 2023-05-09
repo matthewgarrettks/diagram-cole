dd5ffd20e9d6b724cc70f42c45bdaf32b6607e66
Matt Garrett <86023244+matthewgarrettks@users.noreply.github.com>
2023-05-09 10:23:24 -0500

    docblocks

--- src/Command/ScrapeCommand.php
+++ src/Command/ScrapeCommand.php
@@ -2,9 +2,6 @@
 
 namespace App\Command;
 
-// src/Command/CreateUserCommand.php
-namespace App\Command;
-
 use App\Service\Scrape;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -12,8 +9,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-// the "name" and "description" arguments of AsCommand replace the
-// static $defaultName and $defaultDescription properties
 #[AsCommand(
   name: 'app:scrape',
   description: 'Runs the diagram scraper.',
@@ -27,21 +22,36 @@ class ScrapeCommand extends Command
    */
   private Scrape $scrape;
 
+  /**
+   * Constructor, pass in the scrape service
+   * @param  \App\Service\Scrape  $scrape
+   * @param  string|null  $name
+   */
   public function __construct(Scrape $scrape, string $name = null)
   {
     parent::__construct($name);
     $this->scrape = $scrape;
   }
 
+  /**
+   * Configure the console command
+   * @return void
+   */
   public function configure(): void
   {
     $this->addArgument('models', InputArgument::REQUIRED, 'Comma separated list of models');
   }
 
+  /**
+   * Execute console command
+   * @param  \Symfony\Component\Console\Input\InputInterface  $input
+   * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+   * @return int
+   */
   protected function execute(InputInterface $input, OutputInterface $output): int
   {
+    // Grab the equipment models from the command line, comma separated
     $models = explode(",", $input->getArgument("models"));
-    print_r($models);
     if(count($models) < 1){
       $output->writeln("Please include at least one model to scrape.");
       die();
@@ -49,6 +59,4 @@ class ScrapeCommand extends Command
     $output->writeln('Scrape run successfully!');
     return Command::SUCCESS;
   }
-
-
 }
